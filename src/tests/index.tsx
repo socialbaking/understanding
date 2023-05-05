e34417967009eed3d709532f228c45454e833c1f
Fabian Cook <hello@fabiancook.dev>
2023-05-06 09:31:24 +1200

    end test spans

--- src/tests/index.tsx
+++ src/tests/index.tsx
@@ -3,13 +3,16 @@ import {tracer} from "../trace";
 import {shutdown} from "../tracing";
 
 try {
-  await tracer.startActiveSpan("tests", async () => {
-    await tracer.startActiveSpan("client-tests", async () => {
+  await tracer.startActiveSpan("tests", async (span) => {
+    await tracer.startActiveSpan("client-tests", async (span) => {
       await import("./client");
+      span.end();
     });
-    await tracer.startActiveSpan("structured-tests", async () => {
+    await tracer.startActiveSpan("structured-tests", async (span) => {
       await import("./structured");
+      span.end();
     });
+    span.end();
   })
   console.log("Tests successful");
 } catch (error) {
